e2f1acb85ddb1d7f192b97afee2ac91e6b06ebfc
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-12-27 14:54:33 +0100

    chore: add connection to remote mongoDB

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
+const mongoose = require('mongoose');
 const app = require('./app');
+
+const DB = process.env.MONGO_DB.replace(
+  '<PASSWORD>',
+  process.env.MONGO_DB_PASSWORD,
+);
+
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
 const PORT = process.env.PORT || 3000;
 
