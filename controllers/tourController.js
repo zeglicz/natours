9722357531bd57457c812c5a5ec9682d5bcefa72
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-12-27 17:34:42 +0100

    refactor: create MVC Architecture

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,22 +1,4 @@
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
-);
-
-exports.checkID = (req, res, next, value) => {
-  console.log(value);
-
-  const id = Number(value);
-  const tour = tours.find((t) => t.id === id);
-
-  if (!tour)
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  next();
-};
+const Tour = require('../models/tourModel');
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price)
@@ -31,39 +13,26 @@ exports.getAllTours = (req, res) => {
   console.log(req.reqestTime);
   res.status(200).json({
     status: 'success',
-    results: tours.length,
-    requestedAt: req.reqestTime,
-    data: { tours },
+    // results: tours.length,
+    // requestedAt: req.reqestTime,
+    // data: { tours },
   });
 };
 
 exports.getTour = (req, res) => {
-  const id = Number(req.params.id);
-  const tour = tours.find((t) => t.id === id);
-
-  res.status(200).json({
-    status: 'success',
-    data: { tour },
-  });
+  // const id = Number(req.params.id);
+  // const tour = tours.find((t) => t.id === id);
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: { tour },
+  // });
 };
 
 exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  // eslint-disable-next-line prefer-object-spread
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: { tour: newTour },
-      });
-    },
-  );
+  res.status(201).json({
+    status: 'success',
+    // data: { tour: newTour },
+  });
 };
 
 exports.updateTour = (req, res) => {
---
+++ models/tourModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+module.exports = Tour;
+
+// const testTour = new Tour({
+//   name: 'The Park Camper',
+//   // rating: 4.7,
+//   price: 997,
+// });
+
+// testTour
+//   .save()
+//   .then((doc) => console.log(doc))
+//   .catch((err) => console.error(err));
