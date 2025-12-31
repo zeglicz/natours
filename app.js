9cefe876ad345bd869b2e78584e6f825746e4f85
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-12-31 23:21:16 +0100

    feat: add global error handling middleware

--- app.js
+++ app.js
@@ -21,9 +21,26 @@ app.use('/api/v1/users', userRouter);
 
 // the position in the code matters
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`,
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server`,
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+// When we use 4 argument express recognize this as a error handleing middleware and only call it if it's error
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
