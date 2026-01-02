30772605a5bbebb9548898bc2dfff838e3d7b665
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2026-01-03 00:17:33 +0100

    feat: intorduce changedPasswordAfter to check if user changed password after JWT was created; add to protect checker if user still exists

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
@@ -16,6 +17,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
+    passwordChangeAt: req.body.passwordChangeAt,
   });
 
   const token = signToken(newUser._id);
@@ -65,11 +67,27 @@ exports.protect = catchAsync(async (req, res, next) => {
     return next(
       new AppError('Your are not logged in! Please log in to get access', 401),
     );
+
   // 2. Verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
   // 3. Check if user still exists
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser)
+    return next(
+      new AppError(
+        'The user belonging to this token does no longer exist.',
+        401,
+      ),
+    );
 
   // 4. Check if user changed password after the JWToken was issued
+  if (currentUser.changedPasswordAfter(decoded.iat))
+    return next(
+      new AppError('User recently changed password! Please log in again!', 401),
+    );
 
+  // GRANT ACCESS TO PROTECTED ROUTE
+  req.user = currentUser;
   next();
 });
