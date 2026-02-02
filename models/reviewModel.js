015b9175940ea5213282a1e14c1758dcc6fd36bb
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2026-02-02 22:39:15 +0100

    feat: introduce static method to create stats of the avg ratings and num ratings for the tourId when new review was created

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -42,6 +43,34 @@ reviewSchema.pre(/^find/, async function () {
     select: 'name',
     // options: { virtuals: false },
   });
+});
+
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  // this point to the model
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+
+  console.log(stats);
+
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].nRating,
+    ratingsAverage: stats[0].avgRating,
+  });
+};
+
+reviewSchema.post('save', async function (next) {
+  // this point to current review | this.constructor point to model Review
+  this.constructor.calcAverageRatings(this.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
