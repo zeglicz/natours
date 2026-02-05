2e5e33e904ff08099b31ee6f8b9bdf9fa57e9a76
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2026-02-05 14:49:31 +0100

    feat: create new router for tour /tour/:slug and create template for tour.pug 1/2

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -3,12 +3,7 @@ const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
-  // 1. Get tour data from collection
   const tours = await Tour.find();
-
-  // 2. Build template
-
-  // 3. Render that template using tour data from 1.
 
   res.status(200).render('overview', {
     title: 'All Tours',
@@ -16,8 +11,14 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
+    tour,
   });
-};
+});
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,6 +5,6 @@ const viewController = require('../controllers/viewController');
 const router = express.Router();
 
 router.get('/', viewController.getOverview);
-router.get('/tour', viewController.getTour);
+router.get('/tour/:slug', viewController.getTour);
 
 module.exports = router;
