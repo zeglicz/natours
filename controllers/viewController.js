b272be30222877687eda5e79fd35ec1df511fae6
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2026-02-06 21:23:12 +0100

    feat: include map on tour.pug with Mapbox

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -18,7 +18,7 @@ exports.getTour = catchAsync(async (req, res) => {
   });
 
   res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
+    title: `${tour.name} Tour`,
     tour,
   });
 });
