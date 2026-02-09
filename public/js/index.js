96ed31419b863473d9448ac6b67eebfbf390cab4
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2026-02-09 23:29:06 +0100

    feat: update user data via API /updateMe

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,13 @@ import '@babel/polyfill';
 
 import { login, logout } from './login';
 import { displayMap } from './mapbox';
+import { updateData } from './updateSettings';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutButton = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
 
 // DELEGATION
 if (mapBox) {
@@ -25,3 +27,12 @@ if (loginForm)
   });
 
 if (logOutButton) logOutButton.addEventListener('click', logout);
+
+if (userDataForm)
+  userDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateData(name, email);
+  });
