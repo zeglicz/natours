d478ed5ad7e34f9bcfed1f99050b3871468a17aa
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2026-02-10 22:47:14 +0100

    feat: allow to upload multiple tour images

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,39 @@
-const Tour = require('../models/tourModel');
+const multer = require('multer');
+const sharp = require('sharp');
 
+const Tour = require('../models/tourModel');
 const factory = require('./handleFactory');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (!file.mimetype.startsWith('image'))
+    cb(new AppError('Not an image! Please upload only images.'), false);
+
+  cb(null, true);
+};
+
+// const upload = multer({ dest: 'public/img/users' });
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadTourImages = upload.fields([
+  { name: 'imageCover', maxCount: 1 },
+  { name: 'images', maxCount: 3 },
+]);
+
+// upload.single('image'); // req.file
+// upload.array('images', 5); // req.files
+
+exports.resizeTourImages = (req, res, next) => {
+  console.log(req.files);
+
+  next();
+};
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -40,6 +40,8 @@ router
   .post(
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
+    tourController.uploadTourImages,
+    tourController.resizeTourImages,
     tourController.createTour,
   );
 
