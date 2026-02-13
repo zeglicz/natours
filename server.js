24d54eae75646f3bc18251292ed140baaa9bb1cf
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2026-02-13 14:56:07 +0100

    feat: handle SIGTERM for graceful shutdown

--- server.js
+++ server.js
@@ -49,3 +49,10 @@ process.on('unhandledRejection', (err) => {
   });
   // In real life - Restart server now
 });
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('Process terminated!');
+  });
+});
